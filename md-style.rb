0754cf02c4f66b2d1122bbf290a67d3b42fd03fd
Sam Soffes <sam@soff.es>
2021-01-01 07:36:56 -0800

    Update style

--- md-style.rb
+++ md-style.rb
@@ -1,8 +1,23 @@
+# Enable all rules by default
 all
+
+# Disable line length
 exclude_rule 'MD013'
+
+# Disable requiring output after shell commands
 exclude_rule 'MD014'
-exclude_rule 'MD029'
+
+# Headers can’t end in the follow punctuation
+rule 'MD026', punctuation: ',;:'
+
+# Ordered lists should be ascending
+rule 'MD029', style: 'ordered'
+
+# Allow inline HTML
 exclude_rule 'MD033'
 
-# TODO: Only disable for H1
-exclude_rule 'MD026'
+# Disabled because it finds false positives of bare URLs in inline HTML
+exclude_rule 'MD034'
+
+# TODO: Create rule for space between fence and language
+# TODO: Create rule for no front matter `title`
